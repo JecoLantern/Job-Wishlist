f056ea5de8b0f7de3a01aa4a8fe66edfad3c84c2
JecoLantern <jbayonke@hotmail.com>
2020-08-07 14:58:37 -0700

    sql files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE jobWishList_db;
+
+INSERT INTO jobs (company_name, job_title) VALUES ('Pathrise', 'Web developer');
+INSERT INTO jobs (company_name, job_title) VALUES ('AirBnB', 'Web developer');
+INSERT INTO jobs (company_name, job_title) VALUES ('Google', 'Software Engineer');
+INSERT INTO jobs (company_name, job_title) VALUES ('Facebook', 'Web developer');
